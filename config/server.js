b06e32126a093651c38369993b8ad4267fc36a5d
Andres <aneduardotejada@gmail.com>
2021-05-06 06:58:40 -0500

    backend de strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '156996aa00068b856c10571e6d49ef61'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '94edf486-14ea-48c5-8ab1-fdbc3324bfb4'
+};
